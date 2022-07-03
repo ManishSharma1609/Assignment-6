58847cdfcea2f26351cbe7362b4637208e66e680
ManishSharma1609 <104347990+ManishSharma1609@users.noreply.github.com>
2022-07-03 15:51:46 +0530

    Add files via upload

---
+++ Outout6.py
@@ -0,0 +1,50 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/Dell/Downloads/Output.py ==================
+Enter a number: 8
+8  is not a perfect number
+
+Enter a String: NAMAN
+NAMAN is a palindrome
+
+Enter a number: 6
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+Enter a String: the quick brown fox jumps over a lazy hazy dazy dog
+Yes 'the quick brown fox jumps over a lazy hazy dazy dog' is a panagram
+
+Enter a hyphen separated sentence: Manish-Raghav-Auchitya-Rakshit-Aayush
+Aayush-Auchitya-Manish-Raghav-Rakshit
+
+student name:  Manish
+student branch:  Civil
+student id:  21102048
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+Enter number of elements : 6
+-15
+7
+-3
+10
+-2
+3
+No triplets exist
+None
+
+Enter the sequence of parantheses : (({}[]
+(({}[] - is unbalanced
+
